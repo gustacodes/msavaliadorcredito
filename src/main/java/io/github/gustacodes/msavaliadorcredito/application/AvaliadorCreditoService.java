1e4c872ccc859cacec65cda0e6531eb4f18ef8f7
Gustavo Henrique <gustalencarvalho@gmail.com>
2025-05-31 11:20:17 -0300

    feat: adicionado cartões ao cliente via FeignClient

--- src/main/java/io/github/gustacodes/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ src/main/java/io/github/gustacodes/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,22 +1,30 @@
 package io.github.gustacodes.msavaliadorcredito.application;
 
+import io.github.gustacodes.msavaliadorcredito.domain.model.CartaoCliente;
 import io.github.gustacodes.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.gustacodes.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.gustacodes.msavaliadorcredito.infra.CartaoResourceClient;
 import io.github.gustacodes.msavaliadorcredito.infra.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clientesClient;
+    private final CartaoResourceClient cartaoResourceClient;
 
     public SituacaoCliente obterSituacaoCliente(String cpf) {
         ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosDoCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesCliente = cartaoResourceClient.getCartoesByCliente(cpf);
+
         return SituacaoCliente.builder()
                 .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesCliente.getBody())
                 .build();
     }
 
---
+++ src/main/java/io/github/gustacodes/msavaliadorcredito/infra/CartaoResourceClient.java
@@ -0,0 +1,17 @@
+package io.github.gustacodes.msavaliadorcredito.infra;
+
+import io.github.gustacodes.msavaliadorcredito.domain.model.CartaoCliente;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartaoResourceClient {
+
+    @GetMapping
+    ResponseEntity<List<CartaoCliente>> getCartoesByCliente(@RequestParam String cpf);
+
+}
